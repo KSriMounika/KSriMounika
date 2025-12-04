5893f51dbf932ee64ffc99ed5dcfcf970b872fee
Sri Mounika <23P31A4235@acet.ac.in>
2025-12-04 21:51:12 +0530

    Time: 0 ms (100%), Space: 42.2 MB (11.3%) - LeetHub

---
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int distinctIntegers(int n) {
+        if (n==1) return 1;
+        else 
+        {
+            int res = n-1;
+            return res;
+        }
+    }
+}
