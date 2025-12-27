0a3c91ef9637d135a25e32d34a7e7842dff35ad0
Sri Mounika <keerthisrimounika@gmail.com>
2025-12-27 20:10:12 +0530

    Time: 0 ms (100%), Space: 44.1 MB (17.15%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,20 +1,55 @@
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+
+
+//         int m = matrix.length;
+//         int n = matrix[0].length;
+//         for(int i=0; i<m; i++)
+//         {
+//             for(int j=0; j<n; j++)
+//             {
+//                 if(matrix[i][j]== target)
+//                 {
+//                     return true;
+//                 }
+//             }
+//         }
+
+//         return false;
+
+        
+
+
+//     }
+// }
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 
 
         int m = matrix.length;
         int n = matrix[0].length;
-        for(int i=0; i<m; i++)
+        int i=0;
+        int j =n-1;
+        while(i<m && j>=0)
         {
-            for(int j=0; j<n; j++)
+            
+            if(matrix[i][j]> target)
             {
-                if(matrix[i][j]== target)
-                {
-                    return true;
-                }
+                   j--;
             }
-        }
+            else if(matrix[i][j]==target)
+            {
 
+             return true;
+            }
+            else
+            {
+                    i++;
+            }
+
+            
+        }
+       
         return false;
 
         
