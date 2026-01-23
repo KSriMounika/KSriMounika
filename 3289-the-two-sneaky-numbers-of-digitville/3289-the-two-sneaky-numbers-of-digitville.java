d9412512f7fda1350f176cb889bc54c67759f2be
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-01-23 15:14:37 +0530

    Time: 1 ms (99.87%), Space: 45.6 MB (88.38%) - LeetHub

---
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+
+        int n = nums.length;
+        int [] arr = new int[2];
+        int c =0;
+        for(int i=0; i<n; i++)
+        {
+           for(int j = i+1; j<n; j++)
+           {
+            
+                if(nums[i]==nums[j])
+                {
+                 
+                  arr[c++] = nums[i];
+                
+                   
+                
+                }
+           }
+        
+       
+        }
+        return arr;
+       
+        
+    }
+}
