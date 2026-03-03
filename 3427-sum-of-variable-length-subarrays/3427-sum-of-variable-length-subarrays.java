2b19398b0073b4bc6b38f0bdac2c2d1495bce817
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-03-03 15:05:35 +0530

    Time: 1 ms (99.79%), Space: 45.1 MB (93.06%) - LeetHub

---
+++ 3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraySum(int[] nums) {
+
+        int n = nums.length;
+        int s =0;
+        for(int i=0; i<n; i++)
+        {
+            int start = Math.max(0, i-nums[i]);
+            for(int j=start; j<=i; j++)
+            {
+               s += (nums[j]);
+            }
+        }
+        return s;
+        
+    }
+}
