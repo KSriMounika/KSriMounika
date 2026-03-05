9413d8afd007e46efeb1f8a85f35b522c4e38bb3
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-03-05 15:32:47 +0530

    Time: 0 ms (100%), Space: 44.1 MB (26.35%) - LeetHub

---
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int returnToBoundaryCount(int[] nums) {
+
+      
+        int s=0;
+        int c=0;
+        for(int i=0; i<nums.length; i++)
+        {
+            s += nums[i];
+             if(s==0)
+             {
+               c++;
+             }
+        }
+        return c;
+        
+    }
+}
