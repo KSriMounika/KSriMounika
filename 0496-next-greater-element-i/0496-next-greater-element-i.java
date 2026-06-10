664acd07da99fe12222e4aaa769e96d92ed781d5
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-06-10 16:02:01 +0530

    Time: 2 ms (99.38%), Space: 45.5 MB (22.1%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -2,31 +2,33 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
         int[] arr = new int[nums1.length];
-         for(int i=0; i<nums1.length; i++)
-         { 
-            arr[i] = -1;
-           
-            for(int j=0; j<nums2.length-1; j++)
+        int[] st = new int[nums2.length];
+        HashMap<Integer, Integer> h = new HashMap<>();
+        int top = -1;
+        for(int i = nums2.length-1; i>=0; i--)
+        {
+             while(top != -1 && st[top]<= nums2[i])
             {
-                
-                if(nums1[i]==nums2[j])
-                {
-                    for(int k=j+1; k<nums2.length; k++)
-                    {
-                        if(nums2[k] > nums2[j])
-                        {
-                                arr[i] = nums2[k];
-                                break;
-                               
-                        }
-                    }
-                    
-
-                   
-                }
+                top--;
             }
+            if(top == -1)
+            {
+                h.put(nums2[i], -1);
+                st[++top] = nums2[i];
+            }
+            else 
+            {
+               h.put(nums2[i], st[top]);
+               st[++top] = nums2[i];
+            }
+           
+        }
+        for(int i=0; i<nums1.length; i++)
+        {
+            arr[i]= h.get(nums1[i]);
         }
         return arr;
+         
 
 
     }
