baef7120b6c8a843a9e035ccdb44c8e2d4b38c99
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-06-12 13:38:35 +0530

    Time: 2 ms (99.61%), Space: 47.6 MB (92.13%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+
+        int n = nums.length;
+        int[] arr = new int[nums.length];
+        int[] st = new int[nums.length];
+        int top = -1;
+        
+        for(int i=2*nums.length-1; i>=0; i--)
+        {
+        
+          
+            while(top != -1 && st[top] <= nums[i%n])
+            {
+                top--;
+            }
+            if(top == -1)
+            {
+                    arr[i%n] = -1;                   
+            }
+            else
+            {
+                    arr[i%n] = st[top];
+                   
+            }
+            st[++top] = nums[i%n];
+
+            
+        }
+        return arr;
+        
+    }
+}
