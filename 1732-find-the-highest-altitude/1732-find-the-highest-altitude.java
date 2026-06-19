f8f7a9b700a064f2d9c3454ff0cc9dbd8fc76635
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-06-19 15:10:07 +0530

    Time: 0 ms (100%), Space: 42.8 MB (93.81%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,16 +1,17 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-        
-       int a = 0;
-       int m = 0;
-       for(int i=0; i<gain.length; i++)
-       {
-           a += gain[i];
-           m = Math.max(m, a);
-           
-       }
-     
-       return m;
 
+        int m = 0;
+        int a=0;
+        for(int i=0; i<gain.length; i++)
+        {
+                
+                m += gain[i];
+                a = Math.max(a,m);
+
+            
+        }
+        return a;
+        
     }
 }
