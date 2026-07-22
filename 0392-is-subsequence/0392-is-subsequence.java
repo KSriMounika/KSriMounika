a7573e31ac08627544b5e72fae451a29447c37a4
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-07-22 20:00:10 +0530

    Time: 2 ms (73.26%), Space: 43.1 MB (11.39%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+
+      int i=0;
+      int j=0;
+    
+      while(i< t.length() && j<s.length())
+      {
+        if(s.charAt(j) == t.charAt(i))
+        {
+            
+            i++;
+            j++;
+        }
+        else if(s.charAt(j) != t.charAt(i))
+        {
+            i++;
+        }
+
+      }
+      return j == s.length();
+    }
+}
