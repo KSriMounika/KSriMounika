7a565383a2e543b02f3ce28b4fe5de61c73ec79f
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-07-29 19:59:36 +0530

    Time: 0 ms (100%), Space: 42.7 MB (92.93%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,22 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-
-        s = s.trim();
-        int l=0;
-        for(int i=s.length()-1; i>=0; i--)
-        {
-            if(s.charAt(i)!=' ')
-            {
-                l++;
-            }
-            else if(l>0)
-            {
-                break;
-            }
-        }
-      
-        return l;
-
         
+        int c = 0;
+        int i = s.length()-1;
+        while(i>=0 && s.charAt(i)== ' ')
+        {
+            i--;
+        } 
+        while(i>=0 && s.charAt(i) != ' ')
+        {
+            c++;
+            i--;
+        }
+        return c;
     }
 }
