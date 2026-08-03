aa45182cb16d651504cfe718f523553c7879d305
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-08-03 14:49:29 +0530

    Time: 5 ms (61.53%), Space: 46.7 MB (16.65%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -7,17 +7,17 @@ class Solution {
         char[] arr2 = t.toCharArray();
         Arrays.sort(arr);
         Arrays.sort(arr2);
-        HashSet<Character> s1 = new HashSet<>();
-        HashSet<Character> s2 = new HashSet<>();
-        for(char c: arr)
-        {
-            s1.add(c);
-        }
-        for(char c1: arr2)
-        {
-            s2.add(c1);
-        }
-        if(s1.equals(s2) && m==n && Arrays.equals(arr, arr2))
+        // HashSet<Character> s1 = new HashSet<>();
+        // HashSet<Character> s2 = new HashSet<>();
+        // for(char c: arr)
+        // {
+        //     s1.add(c);
+        // }
+        // for(char c1: arr2)
+        // {
+        //     s2.add(c1);
+        // }
+        if( m==n && Arrays.equals(arr, arr2))
         {
             return true;
         }
