d459da5eacdbf361a4a19ec1a2ef18c997f276f9
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-08-07 19:08:31 +0530

    Time: 1 ms (41.05%), Space: 63.2 MB (17.02%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,43 +1,19 @@
-// class Solution {
-//     public void rotate(int[] nums, int k) {
-        
-//         int n = nums.length;
-//         k = k % n; 
-//         int[] temp = new int[n];
-
-       
-//         for (int i = 0; i < k; i++) {
-//             temp[i] = nums[n - k + i];
-//         }
-
-      
-//         for (int i = 0; i < n - k; i++) {
-//             temp[k + i] = nums[i];
-//         }
-
-       
-//         for (int i = 0; i < n; i++) {
-//             nums[i] = temp[i];
-//         }
-
-//     }
-// }
-
 class Solution {
     public void rotate(int[] nums, int k) {
-        k %= nums.length;
-        reverse(nums, 0, nums.length - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
-    }
 
-    public void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
+
+        int[] ans = new int[nums.length];
+        int n = nums.length;
+        for(int i=0; i<nums.length; i++)
+        {
+            int idx = (i + k)%n;
+            ans[idx] = nums[i];
         }
+        for(int i=0; i<nums.length; i++)
+        {
+            nums[i] = ans[i];
+        }
+       
+        
     }
 }
