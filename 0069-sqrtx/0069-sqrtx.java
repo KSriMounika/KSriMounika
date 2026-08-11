55d5099852a789cf40f1c995bf89ac0c8893d5ad
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-08-11 20:55:50 +0530

    Time: 1 ms (97.89%), Space: 42.5 MB (62.14%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,20 +1,31 @@
 class Solution {
     public int mySqrt(int x) {
 
-
-        if(x<2) return x;
-        int low = 1, high = x/2;
-        while(low<=high)
+        int l=1;
+        int h = x;
+        int ans =0;
+        while(l <= h)
         {
-            int mid = low + (high-low)/2;
-
-            long square = (long) mid*mid;
-
-            if(square == x) return mid;
-            else if (square < x) low = mid+1;
-            else high = mid-1;
+            int mid = (l)+(h-l)/2;
+            if((long)mid*mid == x)
+            {
+               ans = mid;
+               break;
+            }
+            else if((long)mid * mid < x)
+            {
+                ans = mid;
+                l = mid+1;
+              
+            }
+            else
+            {
+                h = mid-1;
+                
+            }
         }
-        return high;
+        return ans;
         
+
     }
 }
