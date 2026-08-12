09bf5a1c9fb6708f4a701b2b460f726ffadf4089
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-08-12 19:51:18 +0530

    Time: 0 ms (100%), Space: 44.1 MB (24.61%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,59 +1,31 @@
-// class Solution {
-//     public boolean searchMatrix(int[][] matrix, int target) {
-
-
-//         int m = matrix.length;
-//         int n = matrix[0].length;
-//         for(int i=0; i<m; i++)
-//         {
-//             for(int j=0; j<n; j++)
-//             {
-//                 if(matrix[i][j]== target)
-//                 {
-//                     return true;
-//                 }
-//             }
-//         }
-
-//         return false;
-
-        
-
-
-//     }
-// }
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-
-
+        
         int m = matrix.length;
         int n = matrix[0].length;
-        int i=0;
-        int j =n-1;
-        while(i<m && j>=0)
+       
+        int l=0;
+        int h= m*n-1;
+        while(l <= h)
         {
+                int mid = l + (h-l)/2;
+                int i = mid/n;
+                int j = mid%n;
+                if(matrix[i][j] > target)
+                {
+                    h = mid-1;
+                }
             
-            if(matrix[i][j]> target)
-            {
-                   j--;
-            }
-            else if(matrix[i][j]==target)
-            {
-
-             return true;
-            }
-            else
-            {
-                    i++;
-            }
-
+                else if(matrix[i][j] == target)
+                {
+                    return true;
+                }
+                else
+                {
+                    l = mid+1;
+                }
             
         }
-       
         return false;
-
-        
-
-
     }
 }
