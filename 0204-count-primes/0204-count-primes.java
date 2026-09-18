a8fd26fa73aaf8b7ee185a7d066a4c3cdf7cdba0
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-09-18 12:03:39 +0530

    Time: 749 ms (24.77%), Space: 80 MB (15.77%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,27 +1,30 @@
 class Solution {
-    
     public int countPrimes(int n) {
-
-       if (n <= 2) return 0;
-
-        boolean[] isPrime = new boolean[n];
-        for (int i = 2; i < n; i++) {
-            isPrime[i] = true;
-        }
-
-        for (int i = 2; i * i < n; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    isPrime[j] = false;
+        
+        if(n<2) return 0;
+        boolean[] primes = new boolean[n+1];
+        Arrays.fill(primes, true);
+        primes[0] = false;
+        primes[1] = false;
+        
+        for(int i=2; i<=(int)Math.sqrt(n); i++)
+        {
+           if(primes[i])
+           {
+                for(int j= i*i; j<=n; j+=i)
+                {
+                    primes[j] = false;
                 }
+           }
+        }
+        int c=0;
+        for(int i=0; i<n; i++)
+        {
+            if(primes[i])
+            {
+                c++;
             }
         }
-
-        int count = 0;
-        for (int i = 2; i < n; i++) {
-            if (isPrime[i]) count++;
-        }
-        return count;
-       
+        return c;
     }
 }
