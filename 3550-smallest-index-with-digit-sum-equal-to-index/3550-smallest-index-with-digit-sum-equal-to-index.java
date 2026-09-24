65288ea90c717c508f6ecf12c7b3de65847f0669
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-09-24 12:11:28 +0530

    Time: 1 ms (99.69%), Space: 45.5 MB (77.88%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -1,29 +1,25 @@
 class Solution {
+    public int sumDigits(int n)
+    {
+        int temp = n;
+        int ds = 0;
+        while(temp >0)
+        {
+            ds += temp%10;
+            temp = temp/10;
+        }
+        return ds;
+    }
     public int smallestIndex(int[] nums) {
-
-       
-        int m = Integer.MAX_VALUE;
-       
+        
         for(int i=0; i<nums.length; i++)
         {
-            int temp = nums[i];
-            int s=0;
-            while(temp >0)
+            int res = sumDigits(nums[i]);
+            if(res == i)
             {
-                int n = temp%10;
-                s += n;
-                temp = temp/10;
+                return i;
             }
-             if(s==i)
-              {
-                m = Math.min(m,i);
-              }
-
         }
-        if(m == Integer.MAX_VALUE)
-        {
-            return -1;
-        }
-        return m;
+        return -1;
     }
 }
