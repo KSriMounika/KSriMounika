98751ae14609771f8a11a2325b55fb1c0777caac
Keerthi Sri Mounika <23P31A4235@acet.ac.in>
2026-09-25 11:52:22 +0530

    Time: 1 ms (99.93%), Space: 77 MB (82.65%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,20 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        
-        int counter =0;
-        int Max = nums[0];
-        for(int i=0; i< nums.length; i++)
-        {
-            counter = Math.max(nums[i], nums[i]+counter);
-            Max = Math.max(counter, Max);
-
-        
        
-        if(counter <= 0)
+        int cs = nums[0];
+        int ms = nums[0];
+        for(int i=1; i<nums.length; i++)
         {
-            counter =0;
+            cs = Math.max(nums[i], cs+nums[i]);
+            ms = Math.max(cs, ms);
+
         }
-        }
-        return Max;
+        return ms;
+        
     }
 }
